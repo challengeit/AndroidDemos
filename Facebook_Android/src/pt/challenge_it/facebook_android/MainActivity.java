ed07b412c2c1e06ef2a77184ad0f33646518ced7
dmatoschallenge <diogo.matos@challenge.pt>
2014-01-05 21:05:45 +0000

    POC Android Facebook

---
+++ Facebook_Android/src/pt/challenge_it/facebook_android/MainActivity.java
@@ -0,0 +1,254 @@
+package pt.challenge_it.facebook_android;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.facebook.*;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphPlace;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.*;
+import java.util.*;
+
+/**
+ * This app is based on Facebook Android sample HelloFacebookSample present on FacebookSDK.
+ * 
+ * @source https://developers.facebook.com/docs/android/getting-started/
+ * @author Challenge.IT
+ */
+public class MainActivity extends FragmentActivity {
+
+	private static final String PERMISSION = "publish_actions";
+    private final String PENDING_ACTION_BUNDLE_KEY = "com.facebook.samples.hellofacebook:PendingAction";
+    private Button postStatusUpdateButton;
+    private ProfilePictureView profilePictureView;
+    private TextView greeting;
+    private PendingAction pendingAction = PendingAction.NONE;
+    private GraphUser user;
+    private GraphPlace place;
+    private List<GraphUser> tags;
+    private boolean canPresentShareDialog;
+    private UiLifecycleHelper uiHelper;
+    
+    private enum PendingAction {
+        NONE,
+        POST_STATUS_UPDATE
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(this, new Session.StatusCallback() {
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        });
+        uiHelper.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            String name = savedInstanceState.getString(PENDING_ACTION_BUNDLE_KEY);
+            pendingAction = PendingAction.valueOf(name);
+        }
+
+        setContentView(R.layout.main);
+        ((LoginButton)findViewById(R.id.login_button)).setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+            	MainActivity.this.user = user;
+                updateUI();
+                // It's possible that we were waiting for this.user to be populated in order to post a
+                // status update.
+                handlePendingAction();
+            }
+        });
+
+        profilePictureView = (ProfilePictureView) findViewById(R.id.profilePicture);
+        greeting = (TextView) findViewById(R.id.greeting);
+
+        postStatusUpdateButton = (Button) findViewById(R.id.postStatusUpdateButton);
+        postStatusUpdateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                onClickPostStatusUpdate();
+            }
+        });
+
+        canPresentShareDialog = FacebookDialog.canPresentShareDialog(this,
+                FacebookDialog.ShareDialogFeature.SHARE_DIALOG);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+
+        // Call the 'activateApp' method to log an app event for use in analytics and advertising reporting.  Do so in
+        // the onResume methods of the primary Activities that an app may be launched into.
+        AppEventsLogger.activateApp(this);
+        updateUI();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+        outState.putString(PENDING_ACTION_BUNDLE_KEY, pendingAction.name());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+            @Override
+            public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+                Log.d("HelloFacebook", String.format("Error: %s", error.toString()));
+            }
+
+            @Override
+            public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+                Log.d("HelloFacebook", "Success!");
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (pendingAction != PendingAction.NONE &&
+                (exception instanceof FacebookOperationCanceledException ||
+                exception instanceof FacebookAuthorizationException)) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.cancelled)
+                    .setMessage(R.string.permission_not_granted)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+            pendingAction = PendingAction.NONE;
+        } else if (state == SessionState.OPENED_TOKEN_UPDATED) {
+            handlePendingAction();
+        }
+        updateUI();
+    }
+
+    private void updateUI() {
+        Session session = Session.getActiveSession();
+        boolean enableButtons = (session != null && session.isOpened());
+
+        postStatusUpdateButton.setEnabled(enableButtons || canPresentShareDialog);
+
+        if (enableButtons && user != null) {
+            profilePictureView.setProfileId(user.getId());
+            greeting.setText(getString(R.string.hello_user, user.getFirstName()));
+        } else {
+            profilePictureView.setProfileId(null);
+            greeting.setText(null);
+        }
+    }
+
+    @SuppressWarnings("incomplete-switch")
+    private void handlePendingAction() {
+        PendingAction previouslyPendingAction = pendingAction;
+        // These actions may re-set pendingAction if they are still pending, but we assume they
+        // will succeed.
+        pendingAction = PendingAction.NONE;
+
+        switch (previouslyPendingAction) {
+            case POST_STATUS_UPDATE:
+                postStatusUpdate();
+                break;
+        }
+    }
+
+    private interface GraphObjectWithId extends GraphObject {
+        String getId();
+    }
+
+    private void showPublishResult(String message, GraphObject result, FacebookRequestError error) {
+        String title = null;
+        String alertMessage = null;
+        if (error == null) {
+            title = getString(R.string.success);
+            String id = result.cast(GraphObjectWithId.class).getId();
+            alertMessage = getString(R.string.successfully_posted_post, message, id);
+        } else {
+            title = getString(R.string.error);
+            alertMessage = error.getErrorMessage();
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(alertMessage)
+                .setPositiveButton(R.string.ok, null)
+                .show();
+    }
+
+    private void onClickPostStatusUpdate() {
+        performPublish(PendingAction.POST_STATUS_UPDATE, canPresentShareDialog);
+    }
+
+    private FacebookDialog.ShareDialogBuilder createShareDialogBuilder() {
+        return new FacebookDialog.ShareDialogBuilder(this)
+                .setName("Hello Facebook")
+                .setDescription("The 'Hello Facebook' sample application showcases simple Facebook integration")
+                .setLink("http://developers.facebook.com/android");
+    }
+
+    private void postStatusUpdate() {
+        if (canPresentShareDialog) {
+            FacebookDialog shareDialog = createShareDialogBuilder().build();
+            uiHelper.trackPendingDialogCall(shareDialog.present());
+        } else if (user != null && hasPublishPermission()) {
+            final String message = getString(R.string.status_update, user.getFirstName(), (new Date().toString()));
+            Request request = Request
+                    .newStatusUpdateRequest(Session.getActiveSession(), message, place, tags, new Request.Callback() {
+                        @Override
+                        public void onCompleted(Response response) {
+                            showPublishResult(message, response.getGraphObject(), response.getError());
+                        }
+                    });
+            request.executeAsync();
+        } else {
+            pendingAction = PendingAction.POST_STATUS_UPDATE;
+        }
+    }
+
+    private boolean hasPublishPermission() {
+        Session session = Session.getActiveSession();
+        return session != null && session.getPermissions().contains("publish_actions");
+    }
+
+    private void performPublish(PendingAction action, boolean allowNoSession) {
+        Session session = Session.getActiveSession();
+        if (session != null) {
+            pendingAction = action;
+            if (hasPublishPermission()) {
+                // We can do the action right away.
+                handlePendingAction();
+                return;
+            } else if (session.isOpened()) {
+                // We need to get new permissions, then complete the action when we get called back.
+                session.requestNewPublishPermissions(new Session.NewPermissionsRequest(this, PERMISSION));
+                return;
+            }
+        }
+
+        if (allowNoSession) {
+            pendingAction = action;
+            handlePendingAction();
+        }
+    }
+}
